b813777f6a13a600160e9319e5fa1e5b13549411
sasa-x <sasmithgit@gmail.com>
2024-03-10 16:52:36 +0530

    BranchDTO implement

---
+++ src/main/java/lk/ijse/BookWorm/dto/BranchDTO.java
@@ -0,0 +1,22 @@
+package lk.ijse.BookWorm.dto;
+
+import lk.ijse.BookWorm.entity.Branch;
+
+public class BranchDTO {
+    private String id;
+    private String name;
+    private String location;
+    private String mobile;
+    private String email;
+
+    public Branch toEntity(){
+        Branch branch = new Branch();
+        branch.setId(this.id);
+        branch.setName(this.name);
+        branch.setLocation(this.location);
+        branch.setMobile(this.mobile);
+        branch.setEmail(this.email);
+        return branch;
+    }
+
+}
