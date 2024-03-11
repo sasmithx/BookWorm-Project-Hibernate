15fc7f914c0a425f96f67d73de97a879882c41cd
sasa-x <sasmithgit@gmail.com>
2024-03-11 23:30:38 +0530

    DataValidateController user Validate implement

--- src/main/java/lk/ijse/BookWorm/util/DataValidateController.java
+++ src/main/java/lk/ijse/BookWorm/util/DataValidateController.java
@@ -17,4 +17,25 @@ public class DataValidateController {
         Matcher matcher = pattern.matcher(userName);
         return matcher.matches();
     }
+
+    public static boolean userMobileValidate(String userMobile){
+        String userRegex = "^(?:7|0|(?:\\\\\\\\+94))[0-9]{9,10}$";
+        Pattern pattern = Pattern.compile(userRegex);
+        Matcher matcher = pattern.matcher(userMobile);
+        return matcher.matches();
+    }
+
+    public static boolean emailCheck(String email){
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean addressValidate(String address) {
+        String userRegex = "[A-z @ 0-9]{4,20}";
+        Pattern pattern = Pattern.compile(userRegex);
+        Matcher matcher = pattern.matcher(address);
+        return matcher.matches();
+    }
 }
