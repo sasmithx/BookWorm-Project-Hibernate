f0c81f133df0e06f59470d614522737d1801ef93
sasa-x <sasmithgit@gmail.com>
2024-03-11 22:57:17 +0530

    DataValidateController userIdValidate implement

--- src/main/java/lk/ijse/BookWorm/util/DataValidateController.java
+++ src/main/java/lk/ijse/BookWorm/util/DataValidateController.java
@@ -1,5 +1,13 @@
 package lk.ijse.BookWorm.util;
 
-public class DataValidateController {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class DataValidateController {
+    public static boolean userIdValidate(String userId){
+        String userRegex = "^(U)[0-9]{3}$";
+        Pattern pattern = Pattern.compile(userRegex);
+        Matcher matcher = pattern.matcher(userId);
+        return matcher.matches();
+    }
 }
