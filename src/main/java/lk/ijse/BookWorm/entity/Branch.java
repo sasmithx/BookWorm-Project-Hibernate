c61971eb66d77dd8db06cb14cf050032cf322c05
sasa-x <sasmithgit@gmail.com>
2024-03-11 07:42:07 +0530

    Branch java fixed

---
+++ src/main/java/lk/ijse/BookWorm/entity/Branch.java
@@ -0,0 +1,34 @@
+package lk.ijse.BookWorm.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Cacheable
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Table(name = "Branch")
+public class Branch {
+    @Id
+    @Column(name = "branch_id")
+    private String id;
+
+    @Column(name = "branch_name")
+    private String name;
+
+    @Column(name = "branch_location")
+    private String location;
+
+    @Column(name = "branch_mobile")
+    private String mobile;
+
+    @Column(name = "branch_email")
+    private String email;
+
+}
